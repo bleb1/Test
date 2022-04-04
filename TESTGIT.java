4f03e7f5bbe55b2150007a3ead496b3c4c83ad44
bleb1 <bleb_1@mail.ru>
2022-04-04 20:13:39 +0300

    updating files

--- TESTGIT.java
+++ TESTGIT.java
@@ -3,5 +3,6 @@ package TestGit;
 public class TESTGIT {
     public static void main(String[] args) {
         System.out.println("Ура, я умею работать с гитхабом");
+        System.out.println("jkenfjjdfnkjs");
     }
 }
